6cf766900944da96a8be327cbeb50e2e23955d0c
athp18 <129908089+athp18@users.noreply.github.com>
2024-08-09 22:19:06 -0700

    Trivial: Rearrange function structure

--- modules/orbbec-module/orbbecmodule.cpp
+++ modules/orbbec-module/orbbecmodule.cpp
@@ -11,153 +11,6 @@ class OrbbecModule : public AbstractModule
 {
     Q_OBJECT
 
-public:
-    explicit OrbbecModule(QObject *parent = nullptr)
-        : AbstractModule(parent),
-          m_pipelineStarted(false),
-          m_frameIndex(0),
-          m_fps(30.0),  
-          m_stopped(true)
-    {
-        m_depthOut = registerOutputPort<Frame>(QStringLiteral("depth-out"), QStringLiteral("Depth Frames"));
-        m_irOut = registerOutputPort<Frame>(QStringLiteral("ir-out"), QStringLiteral("IR Frames"));
-    }
-
-    ~OrbbecModule() override 
-    {
-        if (m_pipeline && m_pipelineStarted) {
-            m_pipeline->stop();
-        }
-        safeStopSynchronizer(m_clockSync);
-    }
-
-    ModuleFeatures features() const override
-    {
-        return ModuleFeature::REALTIME | ModuleFeature::SHOW_SETTINGS;
-    }
-
-    ModuleDriverKind driver() const override
-    {
-        return ModuleDriverKind::THREAD_DEDICATED;
-    }
-
-    bool prepare(const TestSubject &) override
-    {
-        try {
-            m_pipeline = std::make_shared<ob::Pipeline>();
-            m_config = std::make_shared<ob::Config>();
-
-            auto depthProfile = m_pipeline->getStreamProfileList(OB_SENSOR_DEPTH)->getVideoStreamProfile(640, OB_HEIGHT_ANY, OB_FORMAT_Y16, 30);
-            auto irProfile = m_pipeline->getStreamProfileList(OB_SENSOR_IR)->getVideoStreamProfile(640, OB_HEIGHT_ANY, OB_FORMAT_Y16, 30);
-
-            m_config->enableStream(depthProfile);
-            m_config->enableStream(irProfile);
-
-            m_depthOut->setMetadataValue("framerate", m_fps);
-            m_irOut->setMetadataValue("framerate", m_fps);
-
-            m_depthOut->start();
-            m_irOut->start();
-
-        } catch (const ob::Error& e) {
-            raiseError(QStringLiteral("Orbbec initialization error: %1").arg(e.getMessage()));
-            return false;
-        }
-
-        // set up clock synchronizer
-        m_clockSync = initClockSynchronizer(m_fps);
-        m_clockSync->setStrategies(TimeSyncStrategy::SHIFT_TIMESTAMPS_BWD);
-
-        m_lastMasterTimestamp = microseconds_t(0);
-        m_lastDeviceTimestamp = microseconds_t(0);
-        // start the synchronizer
-        if (!m_clockSync->start()) {
-            raiseError(QStringLiteral("Unable to set up clock synchronizer!"));
-            return false;
-        }
-
-        return true;
-    }
-
-    void start() override
-    {
-        statusMessage("Acquiring frames...");
-        AbstractModule::start();
-    }
-
-    void runThread(OptionalWaitCondition *waitCondition) override
-    {
-        auto fpsLow = false;
-        auto currentFps = m_fps;
-        auto frameRecordFailedCount = 0;
-        m_stopped = false;
-
-        waitCondition->wait(this);
-
-        try {
-            m_pipeline->start(m_config);
-            m_pipelineStarted = true;
-
-            while (m_running) {
-                const auto cycleStartTime = currentTimePoint();
-                std::shared_ptr<ob::FrameSet> frameSet = nullptr;
-                auto frameRecvTime = MTIMER_FUNC_TIMESTAMP(frameSet = m_pipeline->waitForFrames(200));
-
-                if (frameSet == nullptr) {
-                    frameRecordFailedCount++;
-                    if (frameRecordFailedCount > 32) {
-                        m_running = false;
-                        raiseError(QStringLiteral("Too many attempts to record frames have failed. Is the camera connected properly?"));
-                    }
-                    continue;
-                }
-
-                auto depthFrame = frameSet->depthFrame();
-                if (depthFrame) {
-                    processDepthFrame(depthFrame, m_depthOut, frameRecvTime);
-                }
-
-                auto irFrame = frameSet->irFrame();
-                if (irFrame) {
-                    processIRFrame(irFrame, m_irOut, frameRecvTime);
-                }
-
-                const auto totalTime = timeDiffToNowMsec(cycleStartTime);
-                currentFps = static_cast<int>(1 / (totalTime.count() / static_cast<double>(1000)));
-
-                // warn if there is a bigger framerate drop
-                if (currentFps < (m_fps - 2)) {
-                    fpsLow = true;
-                    setStatusMessage(QStringLiteral("<html><font color=\"red\"><b>Framerate (%1fps) is too low!</b></font>")
-                                         .arg(currentFps));
-                } else if (fpsLow) {
-                    fpsLow = false;
-                    statusMessage("Acquiring frames...");
-                }
-            }
-        } catch (const ob::Error& e) {
-            raiseError(QStringLiteral("Orbbec runtime error: %1").arg(e.getMessage()));
-        }
-
-        m_stopped = true;
-    }
-
-    void stop() override
-    {
-        statusMessage("Cleaning up...");
-        AbstractModule::stop();
-
-        while (!m_stopped) {
-        }
-
-        if (m_pipeline && m_pipelineStarted) {
-            m_pipeline->stop();
-            m_pipelineStarted = false;
-        }
-        safeStopSynchronizer(m_clockSync);
-        statusMessage("Camera disconnected.");
-    }
-
 private:
     std::shared_ptr<DataStream<Frame>> m_depthOut;
     std::shared_ptr<DataStream<Frame>> m_irOut;
@@ -223,11 +76,9 @@ private:
 
     cv::Mat processDepthMat(const cv::Mat& depthMat, float scale)
     {
-        // Convert to 8-bit for visualization
         cv::Mat depthVis;
         depthMat.convertTo(depthVis, CV_8UC1, 255.0 / 5000);  // Assuming max depth of 5000mm
 
-        // Apply color map for better visualization
         cv::Mat colorMappedDepth;
         cv::applyColorMap(depthVis, colorMappedDepth, cv::COLORMAP_JET);
 
@@ -244,31 +95,179 @@ private:
 
         return colorMappedIR;
     }
+
+public:
+    explicit OrbbecModule(QObject *parent = nullptr)
+        : AbstractModule(parent),
+          m_pipelineStarted(false),
+          m_frameIndex(0),
+          m_fps(30.0),  
+          m_stopped(true)
+    {
+        m_depthOut = registerOutputPort<Frame>(QStringLiteral("depth-out"), QStringLiteral("Depth Frames"));
+        m_irOut = registerOutputPort<Frame>(QStringLiteral("ir-out"), QStringLiteral("IR Frames"));
+    }
+
+    ~OrbbecModule() override 
+    {
+        if (m_pipeline && m_pipelineStarted) {
+            m_pipeline->stop();
+        }
+        safeStopSynchronizer(m_clockSync);
+    }
+
+    ModuleFeatures features() const override
+    {
+        return ModuleFeature::REALTIME | ModuleFeature::SHOW_SETTINGS;
+    }
+
+    ModuleDriverKind driver() const override
+    {
+        return ModuleDriverKind::THREAD_DEDICATED;
+    }
+
+    bool prepare(const TestSubject &) override
+    {
+        try {
+            m_pipeline = std::make_shared<ob::Pipeline>();
+            m_config = std::make_shared<ob::Config>();
+
+            auto depthProfile = m_pipeline->getStreamProfileList(OB_SENSOR_DEPTH)->getVideoStreamProfile(640, OB_HEIGHT_ANY, OB_FORMAT_Y16, 30);
+            auto irProfile = m_pipeline->getStreamProfileList(OB_SENSOR_IR)->getVideoStreamProfile(640, OB_HEIGHT_ANY, OB_FORMAT_Y16, 30);
+
+            m_config->enableStream(depthProfile);
+            m_config->enableStream(irProfile);
+
+            m_depthOut->setMetadataValue("framerate", m_fps);
+            m_irOut->setMetadataValue("framerate", m_fps);
+
+            m_depthOut->start();
+            m_irOut->start();
+
+        } catch (const ob::Error& e) {
+            raiseError(QStringLiteral("Orbbec initialization error: %1").arg(e.getMessage()));
+            return false;
+        }
+
+        m_clockSync = initClockSynchronizer(m_fps);
+        m_clockSync->setStrategies(TimeSyncStrategy::SHIFT_TIMESTAMPS_BWD);
+
+        m_lastMasterTimestamp = microseconds_t(0);
+        m_lastDeviceTimestamp = microseconds_t(0);
+        if (!m_clockSync->start()) {
+            raiseError(QStringLiteral("Unable to set up clock synchronizer!"));
+            return false;
+        }
+
+        return true;
+    }
+
+    void start() override
+    {
+        statusMessage("Acquiring frames...");
+        AbstractModule::start();
+    }
+
+    void runThread(OptionalWaitCondition *waitCondition) override
+    {
+        auto fpsLow = false;
+        auto currentFps = m_fps;
+        auto frameRecordFailedCount = 0;
+        m_stopped = false;
+
+        waitCondition->wait(this);
+
+        try {
+            m_pipeline->start(m_config);
+            m_pipelineStarted = true;
+
+            while (m_running) {
+                const auto cycleStartTime = currentTimePoint();
+                std::shared_ptr<ob::FrameSet> frameSet = nullptr;
+                auto frameRecvTime = MTIMER_FUNC_TIMESTAMP(frameSet = m_pipeline->waitForFrames(200));
+
+                if (frameSet == nullptr) {
+                    frameRecordFailedCount++;
+                    if (frameRecordFailedCount > 32) {
+                        m_running = false;
+                        raiseError(QStringLiteral("Too many attempts to record frames have failed. Is the camera connected properly?"));
+                    }
+                    continue;
+                }
+
+                auto depthFrame = frameSet->depthFrame();
+                if (depthFrame) {
+                    processDepthFrame(depthFrame, m_depthOut, frameRecvTime);
+                }
+
+                auto irFrame = frameSet->irFrame();
+                if (irFrame) {
+                    processIRFrame(irFrame, m_irOut, frameRecvTime);
+                }
+
+                const auto totalTime = timeDiffToNowMsec(cycleStartTime);
+                currentFps = static_cast<int>(1 / (totalTime.count() / static_cast<double>(1000)));
+
+                if (currentFps < (m_fps - 2)) {
+                    fpsLow = true;
+                    setStatusMessage(QStringLiteral("<html><font color=\"red\"><b>Framerate (%1fps) is too low!</b></font>")
+                                         .arg(currentFps));
+                } else if (fpsLow) {
+                    fpsLow = false;
+                    statusMessage("Acquiring frames...");
+                }
+            }
+        } catch (const ob::Error& e) {
+            raiseError(QStringLiteral("Orbbec runtime error: %1").arg(e.getMessage()));
+        }
+
+        m_stopped = true;
+    }
+
+    void stop() override
+    {
+        statusMessage("Cleaning up...");
+        AbstractModule::stop();
+
+        while (!m_stopped) {
+        }
+
+        if (m_pipeline && m_pipelineStarted) {
+            m_pipeline->stop();
+            m_pipelineStarted = false;
+        }
+        safeStopSynchronizer(m_clockSync);
+        statusMessage("Camera disconnected.");
+    }
 };
 
-QString OrbbecModuleInfo::id() const
+class OrbbecModuleInfo : public AbstractModuleInfo
 {
-    return QStringLiteral("orbbec-cam");
-}
+public:
+    QString id() const override
+    {
+        return QStringLiteral("orbbec-cam");
+    }
 
-QString OrbbecModuleInfo::name() const
-{
-    return QStringLiteral("Orbbec Depth Sensor");
-}
+    QString name() const override
+    {
+        return QStringLiteral("Orbbec Depth Sensor");
+    }
 
-QString OrbbecModuleInfo::description() const
-{
-    return QStringLiteral("Capture depth and IR data with an Orbbec depth sensor");
-}
+    QString description() const override
+    {
+        return QStringLiteral("Capture depth and IR data with an Orbbec depth sensor");
+    }
 
-ModuleCategories OrbbecModuleInfo::categories() const
-{
-    return ModuleCategory::DEVICES;
-}
+    ModuleCategories categories() const override
+    {
+        return ModuleCategory::DEVICES;
+    }
 
-AbstractModule *OrbbecModuleInfo::createModule(QObject *parent)
-{
-    return new OrbbecModule(parent);
-}
+    AbstractModule *createModule(QObject *parent) override
+    {
+        return new OrbbecModule(parent);
+    }
+};
 
 #include "orbbecmodule.moc"
